3a7995246662c3ec5a7d6e6f26513c044a6e6955
yakultbottle <benjaminlwm7983@gmail.com>
2026-04-08 23:27:15 +0200

    Try out lazygit

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -9,6 +9,17 @@ return {
 
   config = function(_, opts)
     require('toggleterm').setup(opts)
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new {
+      cmd = 'lazygit',
+      displayname = 'lazygit',
+      direction = 'tab',
+    }
+
+    vim.keymap.set('n', '<leader>g', function()
+      lazygit:toggle()
+    end, { desc = 'Toggle Lazygit', noremap = true, silent = true })
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
+vim.o.showtabline = 0
 vim.o.number = true
 -- vim.o.relativenumber = true
 vim.o.mouse = 'a'
